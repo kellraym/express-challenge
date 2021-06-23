f0d0c7f01e26d563e1c4a091ee3f9b7338c89886
kellraym <ray.al.kell@gmail.com>
2021-06-23 11:58:11 -0600

    Add required get, still need to add posts

--- app.js
+++ app.js
@@ -7,10 +7,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/students', (req, res) => {
-  res.send(students)
+  let firstName = req.query.search;
+  if (firstName) {
+    let student = students.filter(student => student.name[0].toLowerCase() === firstName.toLowerCase())
+    if (student.length > 0) {
+      res.send(student)
+    } else {
+      res.send(students)
+    }
+  } else {
+    res.send(students);
+  }
 })
+
+app.get('/students/:studentId', (req, res) => {
+  let id = req.params.studentId
+  let student = students.filter(student => student.id === Number(id))
+
+  if (student) {
+    res.send(student)
+  } else {
+    res.send(students)
+  }
+})
+
+app.get('/grades/:studentId', (req, res) => {
+  let id = req.params.studentId
+  let student = students.filter(student => student.id === Number(id))
+  console.log(student[0].grades)
+  if (student) {
+    res.send(student.grades)
+  } else {
+    res.send(students)
+  }
+})
+
+// app.post('/grades')
 
 const port = 3001
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
